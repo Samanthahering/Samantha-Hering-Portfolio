a9efea21a6952a9f8b4ad94911ea3ac5db248f31
Samantha Hering <samanthahering@yahoo.com>
2020-08-03 18:12:33 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+            <h1>Welcome to my awesome portfolio!!</h1>
+        </div>
+        )
+    }
+}
+
+export default App;
