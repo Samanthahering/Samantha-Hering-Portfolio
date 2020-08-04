08d1e12edc5a77832ed6a228181298d5b5ef3d17
Samantha Hering <samanthahering@yahoo.com>
2020-08-04 18:11:12 -0400

    updated side bar

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,33 @@ import SideDrawer from './SideDrawer'
 import Backdrop from './Backdrop'
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            sideDrawerOpen: false
+        }
+        this.drawerToggle = this.drawerToggle.bind(this)
+
+    }
+   
+    drawerToggle(){
+        this.setState({
+            sideDrawerOpen: !this.state.sideDrawerOpen
+        })
+    }
     render(){
+        let sideDrawer;
+        let backdrop;
+
+        if(this.state.sideDrawerOpen){
+            sideDrawer = <SideDrawer/>;
+            backdrop = <Backdrop/>
+        }
         return(
             <React.Fragment>
                 <Navbar/>
-                <SideDrawer/>
-                <Backdrop/>
+                {sideDrawer}
+                {backdrop}
                     <Router>
                         <Switch>
                             <Route exact path='/' component={Home}/>
