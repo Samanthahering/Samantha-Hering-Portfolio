0ac2b0ddbb0bca4e594f362fc45e678fa50a940a
Samantha Hering <samanthahering@yahoo.com>
2020-08-07 17:17:49 -0400

    update: animations

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,12 @@ import React, { Component } from 'react';
 const About = props => {
     
         return(
-            <div className='sub-landing-pgs'  >
-                    <h1>About Page</h1>
+            <div className='sub-landing-pgs'>
+                    <h1 className='page-title'>About Page</h1>
+                    <div>
+                        <img className='picture-of-myself' src='dist/myself.jpeg' alt='Picture of Samantha'></img>
+
+                    </div>
             </div>
         )
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,12 @@ class Home extends Component {
     render(){
         return(
             <div id='landing-page'>
-            <h1>Samantha Hering</h1>
-            <h2>Software Developer</h2>
+                <h1 className='landing-pg-name'>Samantha Hering</h1>
+                <h2 className='landing-pg-job-title'>Software Developer</h2>
             <Button className='landing-page-btn' variant="outline-secondary" size='lg'>Resume</Button>
             <div className='icons'>
-            <a href='https://www.linkedin.com/in/samanthahering/' target='_blank'><i class="fab fa-linkedin"></i></a>
-            <a href='https://github.com/Samanthahering' target='_blank'><i class="fab fa-github"></i></a>
+                <a href='https://www.linkedin.com/in/samanthahering/' target='_blank'><i class="fab fa-linkedin"></i></a>
+                <a href='https://github.com/Samanthahering' target='_blank'><i class="fab fa-github"></i></a>
             </div>
         </div>
         )
