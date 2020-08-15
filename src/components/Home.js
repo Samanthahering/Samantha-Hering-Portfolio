75deb75ff264a274595aec79f8a47b608b135e0c
Samantha Hering <samanthahering@yahoo.com>
2020-08-14 20:03:12 -0400

    update: btns

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
             <div id='landing-page'>
                 <h1 className='landing-pg-name'>Samantha Hering</h1>
                 <h2 className='landing-pg-job-title'>Software Developer</h2>
-            <Button className='landing-page-btn' variant="outline-secondary" size='lg'>Resume</Button>
+            <Button className='landing-page-btn' variant="outline-secondary" size='lg'><a href='' target='_blank'>Resume</a></Button>
             <div className='icons'>
                 <a href='https://www.linkedin.com/in/samanthahering/' target='_blank'><i class="fab fa-linkedin"></i></a>
                 <a href='https://github.com/Samanthahering' target='_blank'><i class="fab fa-github"></i></a>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,42 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 const Projects = props => {
 
         return(
             <div className='sub-landing-pgs'>
-                <h1 className='page-title'>Projects Page!</h1>
+                <h1 className='page-title'>My Work...</h1>
                 <div className='container projects-container'>
                     <div className='row projects-row'>
-                        <div className='col-sm-6 projects-col'>
-                            <h6>Project 1</h6>
-                            <img className='pic' src='dist/myself.jpeg'/>
+                        <div className='col-md-6 projects-col'>
+                            <h6>DevProz</h6>
+                            <img className='projects-pic' src='dist/myself.jpeg'/>
+                            <p>A digital implementations of the card game 'What Do You Meme'</p>
+                            <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Live</a></Button>
+                            <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Github</a></Button>
                         </div>
-                        <div className='col-sm-6 projects-col'>
-                            <h6>Project 2</h6>
-                            <img className='pic' src='dist/myself.jpeg'/>
+                        <div className='col-md-6 projects-col'>
+                            <h6>Never Bored</h6>
+                            <img className='projects-pic' src='dist/myself.jpeg'/>
+                            <p>An ecommerce site that sells board games</p>
+                            <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Live</a></Button>
+                            <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Github</a></Button>
                         </div>
                     </div>
                     <div className='row projects-row'>
-                        <div className='col-sm-6 projects-col'>
-                            <h6>Project 3</h6>
-                            <img className='pic' src='dist/myself.jpeg'/>
+                        <div className='col-md-6 projects-col'>
+                            <h6>Support Group</h6>
+                            <img className='projects-pic' src='dist/myself.jpeg'/>
+                            <p>A resource center for grief support</p>
+                            <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Live</a></Button>
+                            <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Github</a></Button>
                         </div>
-                        <div className='col-sm-6 projects-col'>
-                            <h6>Project 4</h6>
-                            <img className='pic' src='dist/myself.jpeg'/>
+                        <div className='col-md-6 projects-col'>
+                            <h6>Timnastics</h6>
+                            <img className='projects-pic' src='dist/myself.jpeg'/>
+                            <p>A site for freelance gymnastics coaching</p>
+                            <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Live</a></Button>
+                            <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Github</a></Button>
                         </div>
                     </div>
 
