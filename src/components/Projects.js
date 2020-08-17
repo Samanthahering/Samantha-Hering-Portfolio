602333aa9fab744e109c5dd27b689c3ce26335d7
Samantha Hering <samanthahering@yahoo.com>
2020-08-17 15:53:44 -0400

    update: projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,32 +9,32 @@ const Projects = props => {
                 <div className='container projects-container'>
                     <div className='row projects-row'>
                         <div className='col-md-6 projects-col'>
-                            <h6>DevProz</h6>
-                            <img className='projects-pic' src='dist/myself.jpeg'/>
-                            <p>A digital implementations of the card game 'What Do You Meme'</p>
+                            <h4>DevProz</h4>
+                            <img className='projects-pic' src='dist/what-do-you-meme-project.png'/>
+                            <p className='project-paragraph'>A digital implementations of the card game 'What Do You Meme'</p>
                             <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Live</a></Button>
                             <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Github</a></Button>
                         </div>
                         <div className='col-md-6 projects-col'>
-                            <h6>Never Bored</h6>
-                            <img className='projects-pic' src='dist/myself.jpeg'/>
-                            <p>An ecommerce site that sells board games</p>
+                            <h4>Never Bored</h4>
+                            <img className='projects-pic' src='dist/never-bored-project.png'/>
+                            <p className='project-paragraph'>An ecommerce site that sells board games</p>
                             <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Live</a></Button>
                             <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Github</a></Button>
                         </div>
                     </div>
                     <div className='row projects-row'>
                         <div className='col-md-6 projects-col'>
-                            <h6>Support Group</h6>
-                            <img className='projects-pic' src='dist/myself.jpeg'/>
-                            <p>A resource center for grief support</p>
+                            <h4>Support Group</h4>
+                            <img className='projects-pic' src='dist/grief-support-project.png'/>
+                            <p className='project-paragraph'>A resource center for grief support</p>
                             <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Live</a></Button>
                             <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Github</a></Button>
                         </div>
                         <div className='col-md-6 projects-col'>
-                            <h6>Timnastics</h6>
-                            <img className='projects-pic' src='dist/myself.jpeg'/>
-                            <p>A site for freelance gymnastics coaching</p>
+                            <h4>Timnastics</h4>
+                            <img className='projects-pic' src='dist/timnastics-project.png'/>
+                            <p className='project-paragraph'>A site for freelance gymnastics coaching</p>
                             <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Live</a></Button>
                             <Button className='landing-page-btn' variant="outline-secondary"><a href='' target='_blank'>Github</a></Button>
                         </div>
