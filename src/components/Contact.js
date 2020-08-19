fe6596e89143647f792218d6eeedb1f5b8e8ea1a
Samantha Hering <samanthahering@yahoo.com>
2020-08-19 17:55:36 -0400

    update: contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,24 @@ const Contact = props => {
    
         return(
             <div className='sub-landing-pgs'>
-                <h1 className='page-title'>Contact Page!</h1>
+                <h1 className='page-title'>Get in Touch!</h1>
+                <div className='contact-page-info'>
+                <i class="fas fa-map-marker-alt"></i>
+                <p>Belle Mead, NJ</p>
+                </div>
+                <div className='contact-page-info'>
+                <i class="fas fa-mobile-alt"></i>
+                <p>908-967-7563</p>
+                </div>
+                <div className='contact-page-info'>
+                <i class="far fa-envelope"></i>
+                <p>samanthahering11@gmail.com</p>
+                </div>
+                
+                
+
+                
+
                 <Copyright/>
             </div>
         )
