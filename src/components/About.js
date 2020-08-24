f31b05b815af943035d6c11a5725b7c8e968a9a0
Samantha Hering <samanthahering@yahoo.com>
2020-08-24 10:52:31 -0400

    image try

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = props => {
             <div className='sub-landing-pgs'>
                     <h1 className='page-title'>The Journey...</h1>
                     <div className='about-info'>
-                        <img className='picture-of-myself' src='../dist/myself.jpeg' alt='Picture of Samantha'></img>
+                        <img className='picture-of-myself' src='https://boring-wing-4b9199.netlify.app/dist/myself.jpeg' alt='Picture of Samantha'></img>
                         <p className='paragraph-about-myself'>I am a self- motivated full stack software developer with an eagerness to learn! I am coming from a background in Elementary Education and while I loved teaching my students, I was quickly overcome by feelings of stagnation and eagerness to be in a position where I could constantly grow and learn. 
                         I then decided to put my efforts into a new career path and began learning HTML and CSS through Udemy. I started to dive into JavaScript but after I had learned the basics, I realized I needed the help of a structured environment to help me reach the next level and thats when I applied for the Grace Hopper Program at Fullstack Academy!
                         In my new career path, I am looking forward to building really interesting and useful projects! I am now in a field where I will be a forever learner so I’m excited to see my growth and progress as I continue forward.
@@ -37,7 +37,7 @@ const About = props => {
                                     </ul>
                                 </div>
                         </div> 
-                        <img className='picture-of-code-icon' src='../dist/code-icon.png' alt='Picture of Code Icon'></img>
+                        <img className='picture-of-code-icon' src='/dist/code-icon.png' alt='Picture of Code Icon'></img>
                     </div>
                     <Copyright/>
             </div>
