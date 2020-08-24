cdb435b9579c582c985f8726ce3d46a0e490bd32
Samantha Hering <samanthahering@yahoo.com>
2020-08-24 13:10:42 -0400

    link updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
             <div id='landing-page'>
                 <h1 className='landing-pg-name'>Samantha Hering</h1>
                 <h2 className='landing-pg-job-title'>Software Developer</h2>
-                <a href='../dist/resume.pdf'><Button className='landing-page-btn' variant="outline-secondary" size='lg'>Resume</Button></a>
+                <a href='/resume.pdf'><Button className='landing-page-btn' variant="outline-secondary" size='lg'>Resume</Button></a>
             <div className='icons'>
                 <a href='https://www.linkedin.com/in/samanthahering/'><i class="fab fa-linkedin"></i></a>
                 <a href='https://github.com/Samanthahering'><i class="fab fa-github"></i></a>
